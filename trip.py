f6e0b65e2559f95918062cf3edc1bfe874305a1c
heeonme <154102224+heeonme@users.noreply.github.com>
2024-10-11 11:23:38 +0900

    Update trip.py

--- trip.py
+++ trip.py
@@ -10,35 +10,35 @@ st.set_page_config(
 st.title("수학여행 안내사항")
 
 
-image1 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5190.jpg"
-image2 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5191.jpg"
-image3 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5192.jpg"
-image4 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5193.jpg"
-image5 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5194.jpg"
-image6 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5195.jpg"
-image7 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5196.jpg"
-image8 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5197.jpg"
-image9 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5198.jpg"
-image10 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5199.jpg"
-image11 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5200.jpg"
-image12 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5201.jpg"
+image1 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5203.jpg"
+image2 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5204.jpg"
+image3 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5205.jpg"
+image4 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5206.jpg"
+image5 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5207.jpg"
+image6 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5208.jpg"
+image7 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5209.jpg"
+image8 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5210.jpg"
+image9 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5211.jpg"
+image10 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5212.jpg"
+image11 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5213.jpg"
+image12 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5214.jpg"
 
 
 col1, col2 = st.columns(2)
 
+
 with col1:
+    st.image(image1)
+    st.image(image3)
+    st.image(image5)
+    st.image(image7)
+    st.image(image9)
+    st.image(image11)
+
+with col2:
     st.image(image2)
     st.image(image4)
     st.image(image6)
     st.image(image8)
     st.image(image10)
     st.image(image12)
-
-with col2:
-    st.image(image3)
-    st.image(image5)
-    st.image(image7)
-    st.image(image9)
-    st.image(image11)
-    st.image(image1)
-
