28da1f22d8362f6134dc119cf681c2fc8899e666
heeonme <154102224+heeonme@users.noreply.github.com>
2024-10-11 12:33:34 +0900

    Update trip.py

--- trip.py
+++ trip.py
@@ -9,7 +9,7 @@ st.set_page_config(
 
 st.title("수학여행 안내사항")
 
-if st.button("수학여행 U+1F38"):
+if st.button("수학여행🎈"):
     st.balloons()
 
 image1 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5203.jpg"
