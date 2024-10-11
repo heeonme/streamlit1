0072318bc32ffbff8d2b69ff95e75e018aa7817b
heeonme <154102224+heeonme@users.noreply.github.com>
2024-10-11 10:39:52 +0900

    Add files via upload

---
+++ trip.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="수학여행 안내 사항",
+    page_icon="\U0001F680",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+st.write("##수학여행 안내사항")
+
+
+image1 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5190.bmp"
+image2 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5191.bmp"
+image3 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5192.bmp"
+image4 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5193.bmp"
+image5 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5194.bmp"
+image6 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5195.bmp"
+image7 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5196.bmp"
+image8 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5197.bmp"
+image9 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5198.bmp"
+image10 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5199.bmp"
+image11 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5200.bmp"
+image12 = "https://raw.githubusercontent.com/heeonme/streamlit1/main/IMG_5201.bmp"
